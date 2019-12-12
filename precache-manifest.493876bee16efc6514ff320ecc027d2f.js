3c37e23324bed099ef7091c4d8f0efae6c761cdb
Priscilla Alves <Priihalves_@hotmail.com>
2019-12-12 16:19:45 -0300

    Updates

---
+++ precache-manifest.493876bee16efc6514ff320ecc027d2f.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0a69ad86f6dc213746d5d1736816878",
+    "url": "/Abatalha/index.html"
+  },
+  {
+    "revision": "773edac7e7aee318179e",
+    "url": "/Abatalha/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "df28cf229d0258100e02",
+    "url": "/Abatalha/static/css/main.1ca9b188.chunk.css"
+  },
+  {
+    "revision": "773edac7e7aee318179e",
+    "url": "/Abatalha/static/js/2.cd2c9433.chunk.js"
+  },
+  {
+    "revision": "2367876daefc95544428963d91372257",
+    "url": "/Abatalha/static/js/2.cd2c9433.chunk.js.LICENSE"
+  },
+  {
+    "revision": "df28cf229d0258100e02",
+    "url": "/Abatalha/static/js/main.22c1b21a.chunk.js"
+  },
+  {
+    "revision": "db36e60ec1e92028a4a5",
+    "url": "/Abatalha/static/js/runtime-main.91718035.js"
+  },
+  {
+    "revision": "3e68971370c166d3b80e1e95c9f80a46",
+    "url": "/Abatalha/static/media/Peita1.3e689713.png"
+  },
+  {
+    "revision": "fffe24d125f4ba06070d771610f64cbb",
+    "url": "/Abatalha/static/media/braba.fffe24d1.jpg"
+  },
+  {
+    "revision": "205193645f371cc8e4036b37fdb7e806",
+    "url": "/Abatalha/static/media/brabass.20519364.jpg"
+  },
+  {
+    "revision": "72806879e85ad9c12fef9cf2feb39eff",
+    "url": "/Abatalha/static/media/elas.72806879.jpg"
+  },
+  {
+    "revision": "c886615219dc32b0a74e007674fdd6f9",
+    "url": "/Abatalha/static/media/event1.c8866152.png"
+  },
+  {
+    "revision": "26ab1edf9cb3379318f452cca90dfa98",
+    "url": "/Abatalha/static/media/event10.26ab1edf.png"
+  },
+  {
+    "revision": "be884407c1cb0af93cc12bc78f387a50",
+    "url": "/Abatalha/static/media/event11.be884407.png"
+  },
+  {
+    "revision": "6bcd578c3efcd831324a61a50ddb6f55",
+    "url": "/Abatalha/static/media/event12.6bcd578c.png"
+  },
+  {
+    "revision": "4533db5d4bbffce4867136aada7899bc",
+    "url": "/Abatalha/static/media/event13.4533db5d.png"
+  },
+  {
+    "revision": "17141616e46abe2161f5f1a1c94ad367",
+    "url": "/Abatalha/static/media/event2.17141616.png"
+  },
+  {
+    "revision": "b0a0b20ee22db4bde5bee04c06dfc742",
+    "url": "/Abatalha/static/media/event3.b0a0b20e.png"
+  },
+  {
+    "revision": "213a6dbc4ddba8ef5f144a1cb72a3907",
+    "url": "/Abatalha/static/media/event4.213a6dbc.png"
+  },
+  {
+    "revision": "5838498a7c072f004b52c947092ea1b9",
+    "url": "/Abatalha/static/media/event5.5838498a.png"
+  },
+  {
+    "revision": "9d743d9016ac1d9f0c485131e7cccf1e",
+    "url": "/Abatalha/static/media/event6.9d743d90.png"
+  },
+  {
+    "revision": "6ad657344243b8c1826c0a17941e6ba2",
+    "url": "/Abatalha/static/media/event7.6ad65734.png"
+  },
+  {
+    "revision": "85204fef407db1a9b16b019e209b31b9",
+    "url": "/Abatalha/static/media/event8.85204fef.png"
+  },
+  {
+    "revision": "d77f5b9707c3bf3188633b262c7110b5",
+    "url": "/Abatalha/static/media/event9.d77f5b97.png"
+  },
+  {
+    "revision": "9676f993f4e146707c8d55d653de4236",
+    "url": "/Abatalha/static/media/fundo.9676f993.png"
+  },
+  {
+    "revision": "a645e8224517d0aeec48bfac0f5f6e9e",
+    "url": "/Abatalha/static/media/jade.a645e822.png"
+  },
+  {
+    "revision": "b18a0896b9db7f5dba682a4ab7b01a84",
+    "url": "/Abatalha/static/media/jazz.b18a0896.png"
+  },
+  {
+    "revision": "d54bcd9e9cc95e4d148d060d2274dfdd",
+    "url": "/Abatalha/static/media/logo.d54bcd9e.png"
+  },
+  {
+    "revision": "151b5d3b584d1071dd0b3296a2cf1369",
+    "url": "/Abatalha/static/media/mart.151b5d3b.png"
+  },
+  {
+    "revision": "81bb9baa19504ddcdbe7ff7c3ea6fb98",
+    "url": "/Abatalha/static/media/nat.81bb9baa.png"
+  },
+  {
+    "revision": "874a0205f8856a105996bf75fed8b2de",
+    "url": "/Abatalha/static/media/niaray.874a0205.png"
+  },
+  {
+    "revision": "13342eb3156d196a4d78d4f4422bb7d8",
+    "url": "/Abatalha/static/media/peita2.13342eb3.png"
+  },
+  {
+    "revision": "a89c906a8be94828e1cc7e27a31a9847",
+    "url": "/Abatalha/static/media/peita3.a89c906a.png"
+  },
+  {
+    "revision": "f88ae253c8e89e973d7c8a45f767e55a",
+    "url": "/Abatalha/static/media/peita4.f88ae253.png"
+  },
+  {
+    "revision": "7f138472b21826823caa2c87e2868efc",
+    "url": "/Abatalha/static/media/peita5.7f138472.png"
+  },
+  {
+    "revision": "d8d83c4757b6fe5bb095568870d0c240",
+    "url": "/Abatalha/static/media/peita6.d8d83c47.png"
+  },
+  {
+    "revision": "91e5ae799b40be6935343c950c84382b",
+    "url": "/Abatalha/static/media/peita7.91e5ae79.png"
+  },
+  {
+    "revision": "c35e17ab0886f3a7299ecfd4f96b3a4b",
+    "url": "/Abatalha/static/media/peita8.c35e17ab.png"
+  },
+  {
+    "revision": "554a4a3225d48734fb465d0d2820f5ca",
+    "url": "/Abatalha/static/media/peita9.554a4a32.png"
+  }
+]);
