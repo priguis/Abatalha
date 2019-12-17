463d89ddfbef1faee53adacd09bc7bf1e6cb2ed7
Priscilla Alves <Priihalves_@hotmail.com>
2019-12-17 16:46:46 -0300

    Pronto para apresentar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import Navbar from './componentes/Nav';
-import Produtos from './paginas/Produtos';
-import Eventos from './paginas/Eventos';
-import Contatos from './paginas/Contatos';
-import PaginaInicial from './paginas/PaginaInicial';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import FooterPage from './componentes/FooterPage';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+// import 'mdbreact/dist/css/mdb.css';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Navbar from "./componentes/Nav";
+import Produtos from "./paginas/Produtos";
+import Eventos from "./paginas/Eventos";
+import Contatos from "./paginas/Contatos";
+import PaginaInicial from "./paginas/PaginaInicial";
+import FooterPage from "./componentes/FooterPage";
 
 
 function App () {
